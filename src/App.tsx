e09e96c6dedc06bde80fca0c6eaf4166bc94a33b
travistn <travisn714@gmail.com>
2023-02-10 13:52:33 -0800

    Created home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
       <Footer />
     </>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className='h-screen bg-teal'>
+      <header className='flex flex-col items-center bg-cream py-4'>
+        <h1 className='font-bold text-[20px] lg:text-[28px]'>Welcome to ACNH Handbook!</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
