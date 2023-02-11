68f28a2744f5b3f4d1104209ae7cd3f7be4554ce
travistn <travisn714@gmail.com>
2023-02-11 12:51:02 -0800

    Villagers page fetches villagers api data

--- src/pages/Villagers.tsx
+++ src/pages/Villagers.tsx
@@ -1,5 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+
+import VillagerCard from '../components/VillagerCard';
+
 const Villagers = () => {
-  return <div className='min-h-screen bg-teal'></div>;
+  const { data: villagers } = useQuery({
+    queryKey: ['villagers'],
+    queryFn: () =>
+      fetch(`https://api.nookipedia.com/villagers?api_key=${import.meta.env.VITE_API_KEY}`).then(
+        (res) => res.json()
+      ),
+  });
+
+  return (
+    <main className='min-h-screen bg-teal text-[#194c89]'>
+      <div className='flex flex-col pt-8 w-[90%] m-auto'>
+        <VillagerCard villager={villagers?.[1]} />
+      </div>
+    </main>
+  );
 };
 
 export default Villagers;
