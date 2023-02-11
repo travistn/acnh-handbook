95f4cce6c1ce0be778b02888a01ee32f9d7eb88c
travistn <travisn714@gmail.com>
2023-02-10 23:18:04 -0800

    Added links to the navbar for desktop view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,12 +31,24 @@ const Navbar = () => {
             ACNH Handbook
           </h1>
         </header>
+        <ul className='hidden lg:flex flex-row gap-4 ml-10 items-center'>
+          {links.map((link) => (
+            <Link
+              to={link}
+              key={link}
+              className={`text-white capitalize text-[18px] ${
+                location?.pathname.slice(1) === link ? 'border-b-[1px] border-teal' : ''
+              }`}>
+              {link.split('-').join(' ')}
+            </Link>
+          ))}
+        </ul>
         <div className='ml-auto border-[1px] border-white/[.2] px-3 py-2 rounded-md lg:hidden'>
           <AiOutlineMenu className='text-[22px] text-white' onClick={() => setActive(!active)} />
         </div>
       </nav>
       {active && (
-        <section className='w-full bg-very-dark-teal pl-5 pt-2 pb-3 border-t-[1px] border-white/[.05]'>
+        <section className='w-full bg-very-dark-teal pl-5 pt-2 pb-3 border-t-[1px] border-white/[.2]'>
           <ul className='flex flex-col gap-2'>
             {links.map((link) => (
               <Link to={link} key={link} className='text-white capitalize'>
