14f8079c0264295df1822e369dddd733a32a8711
travistn <travisn714@gmail.com>
2023-02-15 14:01:04 -0800

    Created ScrollToTop component which will automatically scroll to top on page change

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import App from './App';
 import './index.css';
+import App from './App';
+import ScrollToTop from './helpers/ScrollToTop';
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        <ScrollToTop />
         <App />
       </QueryClientProvider>
     </BrowserRouter>
