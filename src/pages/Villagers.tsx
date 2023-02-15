01d7b719c9560194536668511b289163054e4921
travistn <travisn714@gmail.com>
2023-02-14 17:37:08 -0800

    Villager page displays all available villagers in New Horizons

--- src/pages/Villagers.tsx
+++ src/pages/Villagers.tsx
@@ -1,9 +1,13 @@
+import { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 import VillagerCard from '../components/VillagerCard';
+import PaginatedItems from '../components/PaginatedItems';
+import Loading from '../components/Loading';
 
 const Villagers = () => {
-  const { data: villagers } = useQuery({
+  const [villagers, setVillagers] = useState([]);
+  const { data, isLoading } = useQuery({
     queryKey: ['villagers'],
     queryFn: () =>
       fetch(
@@ -13,10 +17,17 @@ const Villagers = () => {
       ).then((res) => res.json()),
   });
 
+  if (isLoading) return <Loading isLoading={isLoading} />;
+
   return (
     <main className='min-h-screen bg-teal'>
-      <article className='flex flex-row pt-8 w-[90%] m-auto'>
-        <VillagerCard villager={villagers?.[15]} />
+      <article className='flex flex-row py-8 w-[90%] m-auto flex-wrap gap-8 lg:justify-center lg:w-[80%]'>
+        {villagers?.map((villager: any, index: number) => (
+          <VillagerCard key={villager?.id + index} villager={villager} />
+        ))}
+      </article>
+      <article className='pb-4'>
+        <PaginatedItems items={data} itemsPerPage={9} setItems={setVillagers} />
       </article>
     </main>
   );
